0f154c7a291a8658e34cd568dba0ad1768a0294e
Xkonti <xkonti@xkonti.tech>
2026-03-12 23:26:47 -0400

    Remove board and fimrware variants

--- Firmware/LogicAnalyzer_V2/Shared_Buffers.h
+++ Firmware/LogicAnalyzer_V2/Shared_Buffers.h
@@ -6,11 +6,7 @@
 #include "Event_Machine.h"
 #include "hardware/flash.h"
 
-#if defined (CORE_TYPE_2)
-    #define FLASH_SETTINGS_OFFSET ((4096 * 1024) - FLASH_SECTOR_SIZE)
-#else
-    #define FLASH_SETTINGS_OFFSET ((2048 * 1024) - FLASH_SECTOR_SIZE)
-#endif
+#define FLASH_SETTINGS_OFFSET ((4096 * 1024) - FLASH_SECTOR_SIZE)
 
 #define FLASH_SETTINGS_ADDRESS (XIP_BASE + FLASH_SETTINGS_OFFSET)
 
